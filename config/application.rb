a6f58d7894e27cda0848118c08aba5a8162d6286
rupak <rupak.shukla@ongraph.com>
2014-06-02 16:15:47 +0530

    Add precompiled assets for heroku

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,9 @@ module SampleApp
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
+   # config.assets.initialize_on_precompile = false
+
+   config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
   end
+
 end
