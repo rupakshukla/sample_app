4f09579b5cbbe0fef1f7b0e23d746b27e97940e1
rupak <rupak.shukla@ongraph.com>
2014-06-16 21:04:37 +0530

    Finish till 9.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,35 @@ describe "User pages" do
 
   subject { page }
 
+   describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    it { should have_title('All users') }
+    it { should have_content('All users') }
+
+    describe "pagination" do
+
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+  end
+
 
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-     before do
+    before do
       sign_in user
       visit edit_user_path(user)
     end
@@ -20,7 +45,7 @@ describe "User pages" do
         fill_in "Password",         with: user.password
         fill_in "Confirm Password", with: user.password
         click_button "Save changes"
-      end
+      end 
 
       it { should have_title(new_name) }
       it { should have_selector('div.alert.alert-success') }
